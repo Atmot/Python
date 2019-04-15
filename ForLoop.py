c3d4563634121990eea5e52742dbbbf9e31980ce
Atmot <holgerx@gmail.com>
2019-04-15 19:47:11 +1000

    Create ForLoop.py

---
+++ ForLoop.py
@@ -0,0 +1,25 @@
+# draws shapes with turtle
+import turtle
+wn = turtle.Screen()
+
+jess = turtle.Turtle()
+
+for i in range(3):
+    jess.forward(30)
+    jess.left(120)
+    
+for i in range(4):
+    jess.forward(40)
+    jess.left(90)
+    
+for i in range(6):
+    jess.forward(50)
+    jess.left(60)
+    
+for i in range(8):
+    jess.forward(60)
+    jess.left(360/8)
+    
+print(360/8)
+
+    
