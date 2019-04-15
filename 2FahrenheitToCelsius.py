16ebf0c46763be74291f80c306cbf5eaa99214e2
Atmot <holgerx@gmail.com>
2019-04-15 10:10:53 +1000

    Create 2FahrenheitToCelsius.py

---
+++ 2FahrenheitToCelsius.py
@@ -0,0 +1,9 @@
+# Converts degrees fahrenheit to degrees celsius
+
+fahrenheit_str = input("Please enter degrees Fahrenheit.")
+fahrenheit = int(fahrenheit_str)
+
+celsius = (fahrenheit - 32) * 5/9
+
+print(fahrenheit, "degrees Fahrenheit are equivalent to", celsius, "degrees Celsius.")
+
